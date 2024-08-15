a7402a87acaccbb87f13a7a600c9d2234f4310cf
Ollie <oliver.grubb@hotmail.com>
2024-08-15 12:34:08 +0100

    added ability to start up a new map and added the asw small house map

--- quick_env_setup.sh
+++ quick_env_setup.sh
@@ -1,9 +1,15 @@
+# Example usage: . quick_env_setup.sh altered_map
+
+#!/bin/bash
+
 export TURTLEBOT3_MODEL=waffle
 colcon build --symlink-install
 source install/setup.bash
 . /usr/share/gazebo/setup.sh
-ros2 launch turtlebot3_gazebo house_interior.launch.py &
+export GAZEBO_MODEL_PATH=`pwd`/src/turtlebot3_simulations/turtlebot3_gazebo/models
+ros2 launch turtlebot3_gazebo $1.launch.py &
 sleep 5
-ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/ros2_ws/src/behavior_trees/behavior_trees/resources/altered_map.yaml
+# Ensure map is chosen when running this bash script
+ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/ros2_ws/src/behavior_trees/behavior_trees/resources/$1.yaml
 
 
--- vacuum_planner.sh
+++ vacuum_planner.sh
@@ -1,3 +1,5 @@
+# Example usage . vacuum_planner.sh altered_map -8.6894 7.69215 0.4 -0.7 9 "[1, 1]"
+
 #!/bin/bash
 
 colcon build --symlink-install
@@ -5,4 +7,5 @@ colcon build --symlink-install
 source install/setup.bash
 . /usr/share/gazebo/setup.sh
 
-ros2 run behavior_trees vacuum_planner --ros-args -p map_name:='altered_map' -p start_position_x:=-8.6894 -p start_position_y:=7.69215 -p x_offset:=0.4 -p y_offset:=-0.7 -p map_resolution:=9 -p start_location:="[1, 1]"
+# Select all the parameters when running the vacuum_planner node
+ros2 run behavior_trees vacuum_planner --ros-args -p map_name:=$1 -p start_position_x:=$2 -p start_position_y:=$3 -p x_offset:=$4 -p y_offset:=$5 -p map_resolution:=$6 -p start_location:=$7
